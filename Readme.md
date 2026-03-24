b4083a032bfe9fd07ab3c1a48b20c8dea48dc01c
Navnath Kadam <navnathkadam284@gmail.com>
2026-03-25 00:35:30 +0530

    feat: enhance face detection with MediaPipe integration

--- Readme.md
+++ Readme.md
@@ -15,7 +15,12 @@
 
 ### Detect A Face / Multiple Faces
 1. Haar Cascade (Opencv built in library) (Old)
+    - Answers: whether face or not ?
     - Pre Train Model which tells is it face or not 
+    - Steps:
+        - it has fixed size window assume (24*24) 
+        - it checks for each position of 24*24 and checks whether it is face or not on every shrink
+        - it uses sliding window (top-bottom and left-right check in each window)
 
 2. MediaPipe (Made by Google can detect 467 meshes)
-    -
+    - Answers: exact structure of face (468 face landmarks)
--- face-test.py
+++ face-test.py
@@ -1,4 +1,5 @@
 import cv2 
+import mediapipe as mp 
 
 class HaarCascade:
     """
@@ -44,5 +45,43 @@ class HaarCascade:
         cap.release()
         cv2.destroyAllWindows()
 
-obj1 = HaarCascade()
-obj1._detect_faces()
+# obj1 = HaarCascade()
+# obj1._detect_faces()
+
+class MediaPipeUs:
+    def __init__(self):
+        self.name = "Ndk"
+    
+    def _detect_faces(self):
+        mp_face_mesh = mp.solutions.face_mesh
+        mp_mesh = mp_face_mesh.FaceMesh()
+        
+        cap = cv2.VideoCapture(0)
+        
+        while True:
+            ret , frame = cap.read()
+            
+            if not ret:
+                print("Failed to capture video")
+                break
+            
+            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = mp_mesh.process(rgb)
+            
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    for landmark in face_landmarks.landmark:
+                        x = int(landmark.x * frame.shape[1])
+                        y = int(landmark.y * frame.shape[0])
+                        cv2.circle(frame, (x,y), 1, (0,255,0), -1)
+            
+            cv2.imshow("Camera", frame)
+            
+            if cv2.waitKey(1) == 27:
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
+obj2 = MediaPipeUs()
+obj2._detect_faces()
